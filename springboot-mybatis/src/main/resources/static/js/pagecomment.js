7450202795a41bb0c97d4ed80bb4992b04449f8b
shiguangleng <774963956F@qq.com>
2020-10-26 23:34:54 +0800

    update pagement.js

--- springboot-mybatis/src/main/resources/static/js/pagecomment.js
+++ springboot-mybatis/src/main/resources/static/js/pagecomment.js
@@ -36,33 +36,66 @@
 
 
     // 点击提交按钮提交评论并获取评论做展示
-    $('.layui-btn').click(function () {
+    $('#commit-btn').click(function () {
         var comment = $('#remarkEditor').val();
+
+        var test = window.location.href;
+        var article_id = test.split("?article_id=")[1];
+
         if (!comment) {
             alert("输入内容不能为空！");
         } else {
-            $.post(
-                'blog/comment',
-                {article_review: comment},
-                function (result) {
-                    $.each(function () {
-                        var username = result.get("username")
-                        var comment = result.get("article_review");
-
-                        var s = '<li class="list-group-item">\n' +
-                            '<div>\n' +
-                            '<span>' + username + '</span>\n' +
-                            '</div>\n' +
-                            '<div class="comment_con">\n' +
-                            '<p>' + comment + '</p>\n' +
-                            '</div>\n' +
-                            '</li>'
-                        $('#reviews').append(s);
-                    });
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                async: false,
+                url: "/blog/comment",
+                data: {article_review: comment, article_id: article_id},
+                success: function (result) {
+                    console.log(result.review[1].article_review);
+                    var s = "";
+                    for (var i = o; i <= result.length, i++) {
+                        var t =
+                            "<li class=\"list-group-item\">\n" +
+                            "<div>\n" +
+                            "<span>" + result.review[1].user_name + "</span>\n" +
+                            "</div>\n" +
+                            "<div class=\"comment_con\">\n" +
+                            "<span>" + result.review[1].article_review + "</span>\n" +
+                            "</div>\n" +
+                            "</li>"
+                        s += t;
+                    }
+                    console.log(s);
+                    $('#reviews').html(s);
                 },
-                "json"
-            );
+                error: function () {
+                    alert("异常，请重试！");
+                }
+            });
 
+
+            // $.post(
+            //     'blog/comment',
+            //     {article_review: comment},
+            //     function (result) {
+            //         $.each(function () {
+            //             var username = result.get("username")
+            //             var comment = result.get("article_review");
+            //
+            //             var s = '<li class="list-group-item">\n' +
+            //                 '<div>\n' +
+            //                 '<span>' + username + '</span>\n' +
+            //                 '</div>\n' +
+            //                 '<div class="comment_con">\n' +
+            //                 '<p>' + comment + '</p>\n' +
+            //                 '</div>\n' +
+            //                 '</li>'
+            //             $('#reviews').append(s);
+            //         });
+            //     },
+            //     "json"
+            // );
 
 
         }
